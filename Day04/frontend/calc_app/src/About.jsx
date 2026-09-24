23486ee9354c23114575fe427567f038c225f598
gmaheswaranmca <gmaheswaranmca@gmail.com>
2026-09-24 15:45:29 +0530

    React JS for Fast API apps

---
+++ Day04/frontend/calc_app/src/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+      My name is maheswaran. I am sitting in the ISE III Lab at NIE North 
+      near by KRS Dam.
+    </div>
+    )
+}
---
+++ Day04/frontend/calc_app/src/App.jsx
@@ -0,0 +1,15 @@
+import About from "./About"
+import Square from "./Square"
+
+function App() {
+  return (
+    <>
+      <p>My Application</p>
+      <hr/>
+      <Square/>
+      <hr/>
+    </>
+  )
+}
+
+export default App
